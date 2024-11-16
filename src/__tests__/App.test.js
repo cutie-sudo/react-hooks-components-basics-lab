a924b00b33f3e75ec67b414f81839b69e1652651
cutie-sudo <faith.nguli@student.moringaschool.com>
2024-11-16 10:00:34 +0300

    add components-basics-lab

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,21 +1,9 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import App from "../components/App";
 
 test("renders without errors", () => {
   expect(() => render(<App />)).not.toThrow();
 });
-test("renders the correct child components", () => {
-  const { container } = render(<App />);
 
-  /*
-    Uncomment the line below to see the DOM elements being returned
-    by the App component in your terminal when you run the tests
-  */
-  // screen.debug();
-
-  expect(container.querySelector("nav")).toBeInTheDocument();
-  expect(container.querySelector("#home")).not.toBeInTheDocument();
-  expect(container.querySelector("#about")).not.toBeInTheDocument();
-});
